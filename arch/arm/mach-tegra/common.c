cab6e05947aed19efc4b0ca5c012453d040bb31b
Andreas Jakob <andreasjakob@gmail.com>
2011-11-23 05:53:24 +0100

    Performance tweaks : Cache / AHB prefetch

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -38,6 +38,32 @@
 #define FUSE_SPARE_BIT_18_REG_OFFSET		0x248
 #define FUSE_SPARE_BIT_19_REG_OFFSET		0x24c
 
+#define AHB_ARBITRATION_PRIORITY_CTRL		0x4
+#define   AHB_PRIORITY_WEIGHT(x)	(((x) & 0x7) << 29)
+#define   PRIORITY_SELECT_USB	BIT(6)
+#define   PRIORITY_SELECT_USB2	BIT(18)
+#define   PRIORITY_SELECT_USB3	BIT(17)
+
+#define AHB_GIZMO_AHB_MEM		0xc
+#define   ENB_FAST_REARBITRATE	BIT(2)
+
+#define AHB_GIZMO_USB		0x1c
+#define AHB_GIZMO_USB2		0x78
+#define AHB_GIZMO_USB3		0x7c
+#define   IMMEDIATE	BIT(18)
+
+#define AHB_MEM_PREFETCH_CFG3	0xe0
+#define AHB_MEM_PREFETCH_CFG4	0xe4
+#define AHB_MEM_PREFETCH_CFG1	0xec
+#define AHB_MEM_PREFETCH_CFG2	0xf0
+#define   PREFETCH_ENB	BIT(31)
+#define   MST_ID(x)	(((x) & 0x1f) << 26)
+#define   AHBDMA_MST_ID	MST_ID(5)
+#define   USB_MST_ID	MST_ID(6)
+#define   USB2_MST_ID	MST_ID(18)
+#define   USB3_MST_ID	MST_ID(17)
+#define   ADDR_BNDRY(x)	(((x) & 0xf) << 21)
+#define   INACTIVITY_TIMEOUT(x)	(((x) & 0xffff) << 0)
 
 bool tegra_chip_compare(u32 chip, u32 major_rev, u32 minor_rev)
 {
@@ -94,12 +120,71 @@ void __init tegra_init_cache(void)
 #ifdef CONFIG_CACHE_L2X0
 	void __iomem *p = IO_ADDRESS(TEGRA_ARM_PERIF_BASE) + 0x3000;
 
+	u32 aux_ctrl;
 	writel(0x331, p + L2X0_TAG_LATENCY_CTRL);
 	writel(0x441, p + L2X0_DATA_LATENCY_CTRL);
-	writel(7, p + L2X0_PREFETCH_OFFSET);
+	writel(2, p + L2X0_PWR_CTRL);
 
-	l2x0_init(p, 0x7C080001, 0x8200c3fe);
+	aux_ctrl = readl(p + L2X0_CACHE_TYPE);
+	aux_ctrl = (aux_ctrl & 0x700) << (17-8);
+	aux_ctrl |= 0x7C000001;
+	l2x0_init(p, aux_ctrl, 0x8200c3fe);
 #endif
+}
+static inline unsigned long gizmo_readl(unsigned long offset)
+{
+    return readl(IO_TO_VIRT(TEGRA_AHB_GIZMO_BASE + offset));
+}
+    
+static inline void gizmo_writel(unsigned long value, unsigned long offset)
+{
+    writel(value, IO_TO_VIRT(TEGRA_AHB_GIZMO_BASE + offset));
+}
+
+static void __init tegra_init_ahb_gizmo_settings(void)
+{
+    unsigned long val;
+    
+	val = gizmo_readl(AHB_GIZMO_AHB_MEM);
+        val |= ENB_FAST_REARBITRATE;
+	gizmo_writel(val, AHB_GIZMO_AHB_MEM);
+
+        val = gizmo_readl(AHB_GIZMO_USB);
+	val |= IMMEDIATE;
+        gizmo_writel(val, AHB_GIZMO_USB);
+
+	val = gizmo_readl(AHB_GIZMO_USB2);
+        val |= IMMEDIATE;
+	gizmo_writel(val, AHB_GIZMO_USB2);
+
+	val = gizmo_readl(AHB_GIZMO_USB3);
+	val |= IMMEDIATE;
+	gizmo_writel(val, AHB_GIZMO_USB3);
+
+	val = gizmo_readl(AHB_ARBITRATION_PRIORITY_CTRL);
+	val |= PRIORITY_SELECT_USB | PRIORITY_SELECT_USB2 | PRIORITY_SELECT_USB3
+	| AHB_PRIORITY_WEIGHT(7);
+	gizmo_writel(val, AHB_ARBITRATION_PRIORITY_CTRL);
+
+	val = gizmo_readl(AHB_MEM_PREFETCH_CFG1);
+	val &= ~MST_ID(~0);
+	val |= PREFETCH_ENB | AHBDMA_MST_ID | ADDR_BNDRY(0xc) | INACTIVITY_TIMEOUT(0x1000);
+	gizmo_writel(val, AHB_MEM_PREFETCH_CFG1);
+
+	val = gizmo_readl(AHB_MEM_PREFETCH_CFG2);
+	val &= ~MST_ID(~0);
+	val |= PREFETCH_ENB | USB_MST_ID | ADDR_BNDRY(0xc) | INACTIVITY_TIMEOUT(0x1000);
+	gizmo_writel(val, AHB_MEM_PREFETCH_CFG2);
+
+	val = gizmo_readl(AHB_MEM_PREFETCH_CFG3);
+	val &= ~MST_ID(~0);
+	val |= PREFETCH_ENB | USB3_MST_ID | ADDR_BNDRY(0xc) | INACTIVITY_TIMEOUT(0x1000);
+	gizmo_writel(val, AHB_MEM_PREFETCH_CFG3);
+
+	val = gizmo_readl(AHB_MEM_PREFETCH_CFG4);
+	val &= ~MST_ID(~0);
+	val |= PREFETCH_ENB | USB2_MST_ID | ADDR_BNDRY(0xc) | INACTIVITY_TIMEOUT(0x1000);
+	gizmo_writel(val, AHB_MEM_PREFETCH_CFG4);
 }
 
 void __init tegra_common_init(void)
@@ -125,4 +210,5 @@ void __init tegra_common_init(void)
 	tegra_dma_init();
 	tegra_mc_init();
 	arm_pm_restart = tegra_machine_restart;
+	tegra_init_ahb_gizmo_settings();
 }
